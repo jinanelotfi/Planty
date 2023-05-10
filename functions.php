7df40d131aa061cebac12ace15f75a2242da5a9b
Jinane Lotfi <jinane.lofi@gmail.com>
2023-05-10 20:08:43 +0200

    Ajout theme css, style boutons quantités

--- functions.php
+++ functions.php
@@ -5,4 +5,5 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
     // Chargement du style.css du thème parent Kadence
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/theme.css', array(), filemtime(get_stylesheet_directory() . '/theme.css'));
 }
